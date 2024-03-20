415fa7e2609a0d86c13ad9f94e82225af703238a
b21willu <102600930+b21willu@users.noreply.github.com>
2024-03-20 21:58:05 +0100

    Moved folder
    moved mango folder

--- react-app/src/components/ProductList.js
+++ react-app/src/components/ProductList.js
@@ -2,14 +2,30 @@ import React, { useState, useEffect } from 'react';
 
 const ProductList = () => {
   const [products, setProducts] = useState([]);
-  const [limit, setLimit] = useState(10); // Default limit is 10  
+  const [limit, setLimit] = useState(10); // Default limit is 10
 
   useEffect(() => {
     fetch('http://localhost:3001/api/products')
       .then(response => response.json())
-      .then(data => setProducts(data))
+      .then(data => {
+        // Loop through the products and clean up the image_downloads field
+        const cleanedProducts = data.map(product => {
+          return {
+            ...product,
+            // Remove [ and ' from image_downloads
+            image_downloads: product.image_downloads.replace(/[\[\]']/g, '').split(', ') // Split the string by comma to get an array of image IDs
+          };
+        });
+        setProducts(cleanedProducts);
+      })
       .catch(error => console.error('Error fetching products:', error));
   }, [limit]);
+
+  // Function to handle image load error
+  const handleImageError = () => {
+    console.error('Error loading image');
+    // You can set a state here to display a placeholder image or show an alternative message
+  };
 
   return (
     <div>
@@ -20,7 +36,11 @@ const ProductList = () => {
             <h2>{product.name}</h2>
             <p>{product.description}</p>
             <p>Pris: {product.price} {product.currency}</p>
-            <img src={product.images} alt={product.name} />
+            <img
+              src={require(`./mango/${product.image_downloads[0]}.jpg`)}
+              alt={product.name}
+              className="product-image"
+            />
           </div>
         ))}
       </div>
