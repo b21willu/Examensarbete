25f62c9d78d43914fe5523de8ed80ed895423f06
b21willu <102600930+b21willu@users.noreply.github.com>
2024-04-02 16:50:51 +0200

    Added footer
    Added footer code in footer.component.ts.
    Added footer html in footer.component.html.
    Added styling for footer in footer.component.css.

--- angular-app/src/app/MainContent/main-content.component.ts
+++ angular-app/src/app/MainContent/main-content.component.ts
@@ -1,18 +1,21 @@
 import { Component } from '@angular/core';
 import { Header } from '../Header/header.component'
 import { Navigation } from '../Navigation/navigation.component'
+import { Footer } from '../Footer/footer.component'
 
 @Component({
   selector: 'app-home',
   standalone: true,
   imports: [  
     Header,
-    Navigation
+    Navigation,
+    Footer
   ],
   template: `
     <main>
       <app-navigation></app-navigation>
       <app-header></app-header>
+      <app-footer></app-footer>
     </main>
   `,
   styleUrls: ['./main-content.component.css'],
