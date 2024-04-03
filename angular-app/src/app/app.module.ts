cb3982f7d9e888b13014b1aa0ce43bb7275f63ed
b21willu <102600930+b21willu@users.noreply.github.com>
2024-04-03 11:33:16 +0200

    Created all the components.
    Created all the components including Footer, Header, Navigation, About, Contact, MainContent, ProductDetail, ProductList and ShoppingCart.

--- angular-app/src/app/app.module.ts
+++ angular-app/src/app/app.module.ts
@@ -3,10 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './header/header.component';
+import { ContactComponent } from './contact/contact.component';
+import { AboutComponent } from './about/about.component';
+import { MainContentComponent } from './main-content/main-content.component';
+import { NavigationComponent } from './navigation/navigation.component';
+import { ProductDetailComponent } from './product-detail/product-detail.component';
+import { ProductListComponent } from './product-list/product-list.component';
+import { ShoppingCartComponent } from './shopping-cart/shopping-cart.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    FooterComponent,
+    HeaderComponent,
+    ContactComponent,
+    AboutComponent,
+    MainContentComponent,
+    NavigationComponent,
+    ProductDetailComponent,
+    ProductListComponent,
+    ShoppingCartComponent,
   ],
   imports: [
     BrowserModule,
