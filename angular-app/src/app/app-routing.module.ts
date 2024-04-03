9580d5e5558062fa13af10eae6a7fc5b05f39403
b21willu <102600930+b21willu@users.noreply.github.com>
2024-04-03 17:43:36 +0200

    Added icon package. And fixed path for shopping cart
    Installed icon package (fontawesome).
    Fixed path for shopping-cart in app.routing.module.
    Added CSS styling for product-detail.

--- angular-app/src/app/app-routing.module.ts
+++ angular-app/src/app/app-routing.module.ts
@@ -5,13 +5,15 @@ import { ContactComponent } from './contact/contact.component';
 import { AboutComponent } from './about/about.component';
 import { ProductListComponent } from './product-list/product-list.component';
 import { ProductDetailComponent } from './product-detail/product-detail.component';
+import { ShoppingCartComponent } from './shopping-cart/shopping-cart.component';
 
 const routes: Routes = [
   { path: 'home', component: MainContentComponent },
   { path: 'about', component: AboutComponent },
   { path: 'contact', component: ContactComponent },
   { path: 'products', component: ProductListComponent },
-  { path: 'product/:sku', component: ProductDetailComponent }
+  { path: 'product/:sku', component: ProductDetailComponent },
+  { path: 'shopping-cart', component: ShoppingCartComponent }
 ];
 
 @NgModule({
