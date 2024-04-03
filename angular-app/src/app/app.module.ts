819e0b006dcd5c6db969f30905acd94f814ba9ff
b21willu <102600930+b21willu@users.noreply.github.com>
2024-04-03 12:07:00 +0200

    Added product.service. And added code for product-list.
    Added product.service to handle the HTTP-requests.
    Added code in product-list to get data from API and html/css to visualize the data.

--- angular-app/src/app/app.module.ts
+++ angular-app/src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
+import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { FooterComponent } from './footer/footer.component';
 import { HeaderComponent } from './header/header.component';
@@ -28,7 +29,8 @@ import { ShoppingCartComponent } from './shopping-cart/shopping-cart.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- angular-app/src/app/product-list/product-list.component.ts
+++ angular-app/src/app/product-list/product-list.component.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ProductService } from '../product.service';
 
 @Component({
   selector: 'app-product-list',
   templateUrl: './product-list.component.html',
-  styleUrl: './product-list.component.css'
+  styleUrls: ['./product-list.component.css']
 })
-export class ProductListComponent {
+export class ProductListComponent implements OnInit {
+  products: any[] = [];
 
+  constructor(private productService: ProductService) { }
+
+  ngOnInit(): void {
+    this.fetchProducts();
+  }
+
+  fetchProducts(): void {
+    this.productService.getProducts().subscribe(
+      data => {
+        // Filter out products with null or empty image_downloads
+        const filteredProducts = data.filter(product => product.image_downloads !== null && product.image_downloads.length > 0);
+
+        // Loop through the filtered products and clean up the image_downloads
+        const cleanedProducts = filteredProducts.map(product => {
+          return {
+            ...product,
+            // Remove [ and ' from image_downloads
+            image_downloads: product.image_downloads.replace(/[\[\]']/g, '').split(', ')
+          };
+        });
+        this.products = cleanedProducts;
+      },
+      error => {
+        console.error('Error fetching products:', error);
+      }
+    );
+  }
 }
