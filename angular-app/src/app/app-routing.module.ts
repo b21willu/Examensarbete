edb0ff10a8972565ba57c9b7a7239b5f2f58e95b
b21willu <102600930+b21willu@users.noreply.github.com>
2024-04-03 11:42:51 +0200

    Fixed paths for my different pages.
    Fixed paths in routing.module so navigation links works.

--- angular-app/src/app/app-routing.module.ts
+++ angular-app/src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MainContentComponent } from './main-content/main-content.component';
+import { ContactComponent } from './contact/contact.component';
+import { AboutComponent } from './about/about.component';
+import { ProductListComponent } from './product-list/product-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component: MainContentComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'contact', component: ContactComponent },
+  { path: 'products', component: ProductListComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
