564cef232618726befad7e1047c063b6bf53d716
b21willu <102600930+b21willu@users.noreply.github.com>
2024-04-03 11:28:14 +0200

    Created new angular project. Removed old project.
    Created new angular project, because i realized i created this project wrong. Created new project containing app.modult.ts file.

--- angular-app/README.md
+++ angular-app/README.md
@@ -1,6 +1,6 @@
-# AngularApp
+# AngularApp3
 
-This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 17.3.0.
+This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 17.3.2.
 
 ## Development server
 
--- angular-app/src/app/app.component.ts
+++ angular-app/src/app/app.component.ts
@@ -1,26 +1,10 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { Header } from './Header/header.component';
-import { Navigation } from './Navigation/navigation.component';
-import { Footer } from './Footer/footer.component';
 
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [ 
-    Header,
-    Navigation,
-    Footer,
-    RouterOutlet
-  ],
-  template: `
-    <app-navigation></app-navigation>
-    <app-header></app-header>
-    <router-outlet></router-outlet>
-    <app-footer></app-footer>
-  `,
-  styleUrls: ['./app.component.css']
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'angular-app';
+  title = 'angular-app3';
 }
