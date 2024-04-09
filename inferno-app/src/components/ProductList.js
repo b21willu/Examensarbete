bdfc394a183e1ce2d4a6dcc3658d4b6a1d267889
b21willu <102600930+b21willu@users.noreply.github.com>
2024-04-09 21:02:16 +0200

    Moved folder mango. Added code for ProductList.
    Moved folder mango to src/assets.
    
    Added code for ProductList to retrieve images from database and show them from folder mango.

--- inferno-app/src/components/ProductList.js
+++ inferno-app/src/components/ProductList.js
@@ -4,7 +4,8 @@ class ProductList extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      products: []
+      products: [],
+      images: {} // Förvara bild-URL:er i state
     };
   }
 
@@ -15,9 +16,36 @@ class ProductList extends Component {
   fetchProducts = async () => {
     try {
       const response = await fetch(`http://localhost:3001/api/products`);
-      console.log(response); // Logga response-objektet för att felsöka
       const data = await response.json();
-      this.setState({ products: data });
+  
+      // Skapa ett tomt objekt för att lagra bilder för varje produkt
+      const images = {};
+  
+      // Gå igenom varje produkt för att samla in bilder
+      await Promise.all(data.map(async product => {
+        if (product.image_downloads && typeof product.image_downloads === 'string') {
+          // Rengör strängen för att ta bort [ ] och ''
+          const cleanedImageDownloads = product.image_downloads.replace(/[[\]']/g, '');
+  
+          // Dela upp strängen och hämta alla bilder
+          const imagesList = cleanedImageDownloads.split(',');
+          const imagePromises = imagesList.map(async imageName => {
+            // Dynamiskt importera bilderna
+            try {
+              const { default: image } = await import(`../../../mango/${imageName}.jpg`);
+              return image;
+            } catch (error) {
+              return null;
+            }
+          });
+  
+          // Vänta på alla bilder att hämtas och spara dem i state
+          images[product.sku] = await Promise.all(imagePromises);
+        }
+      }));
+  
+      // Uppdatera state med bilder för varje produkt
+      this.setState({ products: data, images });
     } catch (error) {
       console.error('Error fetching products:', error);
     }
@@ -30,7 +58,14 @@ class ProductList extends Component {
         <div className="product-list">
           {this.state.products.map(product => (
             <div key={product.sku} className="product">
-              <h3>{product.name}</h3>
+              {this.state.images[product.sku] && (
+                <img
+                  src={this.state.images[product.sku][0]}
+                  alt={product.name} 
+                  className="product-content"
+                  id="product-image"
+                 />
+              )}
               <p className="product-content">{product.name}</p>
               <p className="product-content">Pris: {product.price} {product.currency}</p>
             </div>
