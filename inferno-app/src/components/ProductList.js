9cb74e644ccfc3a459c91fbb242575f5a8eb87b7
b21willu <102600930+b21willu@users.noreply.github.com>
2024-04-10 15:33:42 +0200

    Added filter function.
    Added filter function, to filter products based on their category.
    Removed asset folder (not used).

--- inferno-app/src/components/ProductList.js
+++ inferno-app/src/components/ProductList.js
@@ -6,7 +6,8 @@ class ProductList extends Component {
     super(props);
     this.state = {
       products: [],
-      images: {} // Förvara bilder i state
+      images: {}, // Förvara bilder i state
+      filterTerm: '',
     };
   }
 
@@ -16,7 +17,8 @@ class ProductList extends Component {
 
   fetchProducts = async () => {
     try {
-      const response = await fetch(`http://localhost:3001/api/products`);
+      const apiUrl = 'http://localhost:3001/api/products';
+      const response = await fetch(apiUrl);
       const data = await response.json();
   
       // Skapa ett tomt objekt för att lagra bilder för varje produkt
@@ -52,16 +54,54 @@ class ProductList extends Component {
     }
   };
 
+  handleFilterChange = (event) => {
+    const term = event.target.value;
+    this.setState({ filterTerm: term });
+  };
+
+  filterProducts = (product) => {
+    const { filterTerm } = this.state;
+    if (!filterTerm) return true; // Om ingen kategori är vald returnera true, för att visa alla produkter
+    return product.terms.includes(filterTerm);
+  };
+
   render() {
+    const { products, images } = this.state;
+
     return (
-      <div>
+    <div>
+        <div className='filter-container'>
+          <label htmlFor="filter" className="filter-label">Filter:</label>
+          <select id="filter" onChange={this.handleFilterChange} className="filter-select">
+            <option value="">All</option>
+            <option value="backpack">Backpack</option>
+            <option value="bags">Bags</option>
+            <option value="cardigans">Cardigans</option>
+            <option value="cargo">Cargo</option>
+            <option value="dresses">Dresses</option>
+            <option value="hoodies">Hoodies</option>
+            <option value="jackets">Jackets</option>
+            <option value="jeans">Jeans</option>
+            <option value="knitwear">Knitwear</option>
+            <option value="linen">Linen</option>
+            <option value="pants">Pants</option>
+            <option value="puffers">Puffers</option>
+            <option value="shoes">Shoes</option>
+            <option value="shorts">Shorts</option>
+            <option value="skirts">Skirts</option>
+            <option value="suits">Suits</option>
+            <option value="sweaters">Sweaters</option>
+            <option value="t-shirts">T-shirts</option>
+            <option value="tops">Tops</option>
+          </select>
+        </div>
         <div className="product-list">
-          {this.state.products.map(product => (
+          {products.filter(this.filterProducts).map(product => (
             <div key={product.sku} className="product">
-              {this.state.images[product.sku] && (
+              {images[product.sku] && (
                 <Link to={`/product/${product.sku}`}>
                   <img
-                    src={this.state.images[product.sku][0]}
+                    src={images[product.sku][0]}
                     alt={product.name} 
                     className="product-content"
                     id="product-image"
